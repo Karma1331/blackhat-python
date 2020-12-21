c839e1aed955f56dba868f29f71c84bede99d010
Karma1331 <brazuca1331@gmail.com>
2020-12-20 20:14:12 -0600

    working netcat replacement (so cool!!)

--- chapter2/net-tool.py
+++ chapter2/net-tool.py
@@ -1,33 +1,19 @@
+#!/usr/bin/python3
+
 import sys
 import socket
 import getopt
 import threading
 import subprocess
 
-#global variables
-listen                  = False
-command                 = False
-upload                  = False
-execute                 = ""
-target                  = ""
-upload_destination      = ""
-port                    = 0
-
-def usage():
-    print("Net Tool")
-    print("\b")
-    print("Usage: net-tool.py -t host -p port")
-    print("-l --listen                  - listen on [host]:[port] for incoming connections")
-    print("-e --execute=file            - execute the given file upon receiving a connection")
-    print("-c --command                 - initialize shell")
-    print("-u --upload=destination      - upon connecting upload file to [destination]")
-    print("\b\b")
-    print("Example:")
-    print("net-tool.py -t 0.0.0.0 -p 5000 -l -c")
-    print("net-tool.py -t 0.0.0.0 -p 5000 -l -u=c:\\target.exe")
-    print("net-tool.py -t 0.0.0.0 -p 5000 -l -e=\"cat /etc/passwd\"")
-    print("echo 'abcdef' | ./net-tool.py -t 0.0.0.0 -p 135")
-    sys.exit(0)
+# define global variables
+listen = False
+command = False
+upload =False
+execute = ""
+target = ""
+upload_destination = ""
+port = 0
 
 def main():
     global listen
@@ -37,95 +23,95 @@ def main():
     global upload_destination
     global target
 
+    # no arg given, display proper usage
     if not len(sys.argv[1:]):
         usage()
     
-    # read commandline options
+    # interpret switches
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:", ["help","listen","execute","target","port","command","upload"])
+        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:", ["help","listen","execute","target","port","command","upload"]) 
+        for o,a in opts:
+            if o in ("-h","--help"):
+                usage()
+            elif o in ("-l","--listen"):
+                listen = True
+            elif o in ("-e","--execute"):
+                execute = a
+            elif o in ("-c","--command"):
+                command = True
+            elif o in ("-u","--upload"):
+                upload_destination = a
+            elif o in ("-t","--target"):
+                target = a
+            elif o in ("-p","--port"):
+                port = int(a)
+            else:
+                assert False,"Unhandled Option"
     except getopt.GetoptError as err:
         print(str(err))
         usage()
 
-    for o,a in opts:
-    if o in ("-h","--help"):
-        usage()
-    elif o in ("-l","--listen"):
-        listen = True
-    elif o in ("-e","--execute"):
-        execute = a
-    elif o in ("-c","--command"):
-        command = True
-    elif o in ("-u","--upload"):
-        upload_destination = a
-    elif o in ("-t","--target"):
-        target = a
-    elif o in ("-p","--port"):
-        port = int(a)
-    else:
-        assert False,"Unhandled Option"
-
-    # listen or send data from stdin?
     if not listen and len(target) and port > 0:
-
-        # read in the buffer from command line
-        # this will block, CTRL-D to break
+        print("CTRL + D to enter shell")
         buffer = sys.stdin.read()
-
-        # send data
         client_sender(buffer)
-
-    # listen then maybe upload, execute, get shell depending on switches
     if listen:
         server_loop()
 
-def client_sender(buffer):
+# Display possible args
+def usage():
+    print("Usage: net-tool.py -t host -p port")
+    print("-l --listen                  - listen on [host]:[port] for incoming connections")
+    print("-e --execute=file            - execute the given file upon receiving a connection")
+    print("-c --command                 - initialize shell")
+    print("-u --upload=destination      - upon connecting upload file to [destination]")
+    print("-t --target                  - define target to connect to")
+    print("-p --port                    - define port to connect to")
+    print("\b\b")
+    print("Example:")
+    print("net-tool.py -t 0.0.0.0 -p 5000 -l -c")
+    print("net-tool.py -t 0.0.0.0 -p 5000 -l -u=c:\\target.exe")
+    print("net-tool.py -t 0.0.0.0 -p 5000 -l -e=\"cat /etc/passwd\"")
+    print("echo 'abcdef' | ./net-tool.py -t 0.0.0.0 -p 135")
+    sys.exit(0)
 
+# Connect, receive & send data to target
+def client_sender(buffer):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        # connect to target host
-        client.connect((target,port))
-
+        client.connect((target, port))
         if len(buffer):
-            client.sendto(buffer)
-        
+            client.send(buffer.encode('utf-8'))
+
+        # receive, interpret and respond
         while True:
-            #  wait for data back
             recv_len = 1
-            response = ""
-
+            response = b''
             while recv_len:
-
-                data        = client.recv(4096)
-                recv_len    = len(data)
-                response    += data
-
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
                 if recv_len < 4096:
                     break
 
-            print(response)
+            print(response.decode('utf-8'), end=' ')
 
-            # wait for more input
-            buffer = raw_input("")
+            buffer = input("")
             buffer += "\n"
-
-            # send response
-            client.send(buffer)
-
-    except:
-
+            client.send(buffer.encode('utf-8'))
+    except socket.error as exc:
         print("[*] Exception! Exiting.")
-
-        # tear down connection
+        print(f"[*] Caught exception socket.error: {exc}")
         client.close()
 
+# Listener function
 def server_loop():
     global target
 
-    # if no target is defined, listen on all interfaces
+    # if target not defined, listen on all interfaces
     if not len(target):
-        target = "0.0.0.0"         
+        target = "0.0.0.0"
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target, port))
@@ -133,84 +119,59 @@ def server_loop():
 
     while True:
         client_socket, addr = server.accept()
-
-        #start thread to handle new client
-        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
+        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
         client_thread.start()
 
+# Interpret args
 def run_command(command):
-
-    # trim newline
     command = command.rstrip()
 
-    # run command and grab output
+    # run the command, get output
     try:
-        output = subprocess.check_output(command,stderr=subprocess.STDOUT, shell=true)
-    except:
-        output = "failed to execute command.\r\n"
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError as e:
+        output = e.output
 
     return output
 
+# target interaction function
 def client_handler(client_socket):
     global upload
     global execute
     global command
 
-    # check for upload
     if len(upload_destination):
-
-        # read bytes, write to destination
         file_buffer = ""
-
-        # continue reading until no more
         while True:
             data = client_socket.recv(1024)
-
             if not data:
                 break
             else:
                 file_buffer += data
-
-        # IN bytes OUT string
         try:
-            file_descriptor = open(upload_destination,"wb")
-            file_descriptor.write(file_buffer)
+            file_descriptor = open(upload_destination, "wb")
+            file_descriptor.write(file_buffer.encode('utf-8'))
             file_descriptor.close()
+            client_socket.send(f"Successfully saved file to {upload_destination}\r\n")
+        except OSError:
+            client_socket.send(f"Failed to save file to {upload_destination}\r\n")
 
-            # File written notification
-            client_socket.send(f'Successfully saved file to {upload_destination}')
-
-    # check for command execution
+    # check command execution
     if len(execute):
-
         # run command
         output = run_command(execute)
-
         client_socket.send(output)
 
-    # Check if shell was requested
+    # go into loop if shell requested
     if command:
-
         while True:
-
-            client_socket.send("<BHP:#> ")
-
-                # receive until linefeed
-                (enter_key)
-            cmd_buffer = ""
-
-            while "\n" not in cmd_buffer:
-                cmd_buffer += client_socket.recv(1-24)
-
-            # send back output
+            client_socket.send("<BHP:#> ".encode('utf-8'))
+            # receive until enter key pressed
+            cmd_buffer = b''
+            while b"\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+            # send command
             response = run_command(cmd_buffer)
-
-            # send back response
             client_socket.send(response)
 
-
 main()
-
-
-    
-
